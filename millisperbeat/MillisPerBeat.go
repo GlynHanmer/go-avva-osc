d8e54edcd0e035bdc8b8ce9964c55bbb2873e605
Glyn Owen Hanmer <glynhanmer@gmail.com>
2017-01-31 21:57:09 +0000

    MillisPerBeat type

---
+++ millisperbeat/MillisPerBeat.go
@@ -0,0 +1,19 @@
+package millisperbeat
+
+import (
+	"github.com/hypebeast/go-osc/osc"
+)
+
+type MillisPerBeat struct{}
+
+func NewMillisPerBeat() MillisPerBeat {
+	return MillisPerBeat{}
+}
+
+func (c MillisPerBeat) Osc() *osc.Message {
+	return osc.NewMessage("/millisperbeat")
+}
+
+func (c MillisPerBeat) String() string {
+	return "millisperbeat"
+}
---
+++ millisperbeat/MillisPerBeat_test.go
@@ -0,0 +1,55 @@
+package millisperbeat
+
+import (
+	"github.com/hypebeast/go-osc/osc"
+	"testing"
+)
+
+type TestSet struct {
+	expected MillisPerBeat
+}
+
+var MillisPerBeatTestSets = []TestSet{
+	TestSet{MillisPerBeat{}},
+}
+
+func Test_createMillisPerBeat(t *testing.T) {
+	for key, ts := range MillisPerBeatTestSets {
+		actual := NewMillisPerBeat()
+		if actual != ts.expected {
+			t.Fatalf("MillisPerBeatTestSets[%d}: MillisPerBeat returned not as expected.\n\tActual  : %v\n\tExpected: %v", key, actual, ts.expected)
+		}
+	}
+}
+
+type OscTestSet struct {
+	expected *osc.Message
+}
+
+var MillisPerBeatOSCTestTests = []OscTestSet{
+	OscTestSet{osc.NewMessage("/millisperbeat")},
+}
+
+func Test_createCanvasOscMessages(t *testing.T) {
+	for key, ts := range MillisPerBeatOSCTestTests {
+		actual := NewMillisPerBeat().Osc()
+		if !actual.Equals(ts.expected) {
+			t.Fatalf("MillisPerBeatOSCTestTests[%d]: OSC Message generated not as expected:\n\t\n\tActual  : %v\n\tExpected: %v", key, actual, ts.expected)
+		}
+	}
+}
+
+func Test_StringerInterface(t *testing.T) {
+	var testSets = []struct {
+		expected string
+	}{
+		{"millisperbeat"},
+	}
+	for _, ts := range testSets {
+		c := NewMillisPerBeat()
+		actual := c.String()
+		if actual != ts.expected {
+			t.Fatalf("Actual and expected String() output are not as expected\n\tActual  :%s\n\tExpected: %s", actual, ts.expected)
+		}
+	}
+}
