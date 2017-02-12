83d309dc6b56df8031611485839076bb0eec1142
Glyn Owen Hanmer <glynhanmer@gmail.com>
2017-02-12 14:57:39 +0000

    AvvaOscMessageBuilder interface

--- Osc.go
+++ Osc.go
@@ -1,3 +1,5 @@
+// Package osc builds OSC messages to be sent to the avva.studio visuals generating service.
+// osc holds various types that represent entities in the avva.studio visuals system.
 package osc
 
 import (
