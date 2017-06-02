d4278da8d09a17eb1d238238aabadf0e1a9e04a5
Glyn Hanmer <glynhanmer@gmail.com>
2017-06-02 18:13:54 +0100

    Lift added with tests

--- pitterpatter/Opacity_internal_test.go
+++ pitterpatter/Opacity_internal_test.go
@@ -8,43 +8,43 @@ import (
 
 func Test_gainEquals(t *testing.T) {
 		testSets := []struct{
-			gainA *gain
-			gainB *gain
+			A     *gain
+			B     *gain
 			equal bool
 		}{
 			{
-				gainA:nil,
-				gainB:nil,
-				equal:true,
+				A:     nil,
+				B:     nil,
+				equal: true,
 			},
 			{
-				gainA:gainPointer(0.5),
-				gainB:nil,
-				equal:false,
+				A:     gainPointer(0.5),
+				B:     nil,
+				equal: false,
 			},
 			{
-				gainA:gainPointer(0.5),
-				gainB:gainPointer(0.5),
-				equal:true,
+				A:     gainPointer(0.5),
+				B:     gainPointer(0.5),
+				equal: true,
 			},
 			{
-				gainA:gainPointer(0.0),
-				gainB:gainPointer(1.0),
-				equal:false,
+				A:     gainPointer(0.0),
+				B:     gainPointer(1.0),
+				equal: false,
 			},
 			{
-				gainA:nil,
-				gainB:gainPointer(0.5),
-				equal:false,
+				A:     nil,
+				B:     gainPointer(0.5),
+				equal: false,
 			},
 		}
 	for _, testSet := range testSets {
-		equal := testSet.gainA.equals(testSet.gainB)
+		equal := testSet.A.equals(testSet.B)
 		if testSet.equal != equal {
 			var message bytes.Buffer
 			fmt.Fprintf(&message, "Expecting: %t\nActual  : %t", testSet.equal, equal)
-			fmt.Fprintf(&message, "\nA: %s", testSet.gainA)
-			fmt.Fprintf(&message, "\nB: %s\n", testSet.gainB)
+			fmt.Fprintf(&message, "\nA: %s", testSet.A)
+			fmt.Fprintf(&message, "\nB: %s\n", testSet.B)
 			t.Error(message.String())
 		}
 	}
@@ -90,6 +90,94 @@ func Test_Gain(t *testing.T) {
 		}
 		if !gain.equals(testSet.gain) {
 			t.Errorf("Unexpected value.\nExpected: %s\nActual  : %s", testSet.gain, gain)
+		}
+	}
+}
+
+func Test_liftEquals(t *testing.T) {
+	testSets := []struct{
+		A     *lift
+		B     *lift
+		equal bool
+	}{
+		{
+			A:     nil,
+			B:     nil,
+			equal: true,
+		},
+		{
+			A:     liftPointer(0.5),
+			B:     nil,
+			equal: false,
+		},
+		{
+			A:     liftPointer(0.5),
+			B:     liftPointer(0.5),
+			equal: true,
+		},
+		{
+			A:     liftPointer(0.0),
+			B:     liftPointer(1.0),
+			equal: false,
+		},
+		{
+			A:     nil,
+			B:     liftPointer(0.5),
+			equal: false,
+		},
+	}
+	for _, testSet := range testSets {
+		equal := testSet.A.equals(testSet.B)
+		if testSet.equal != equal {
+			var message bytes.Buffer
+			fmt.Fprintf(&message, "Expecting: %t\nActual  : %t", testSet.equal, equal)
+			fmt.Fprintf(&message, "\nA: %s", testSet.A)
+			fmt.Fprintf(&message, "\nB: %s\n", testSet.B)
+			t.Error(message.String())
+		}
+	}
+}
+
+func Test_Lift(t *testing.T) {
+	testSets := []struct {
+		float64
+		lift  *lift
+		error bool
+	}{
+		{
+			float64: -1,
+			lift:    nil,
+			error:   true,
+		},
+		{
+			float64: 0,
+			lift:    liftPointer(0),
+			error:   false,
+		},
+		{
+			float64: 0.5,
+			lift:    liftPointer(0.5),
+			error:   false,
+		},
+		{
+			float64: 1,
+			lift:    liftPointer(1),
+			error:   false,
+		},
+		{
+			float64: 1.5,
+			lift:    nil,
+			error:   true,
+		},
+	}
+
+	for _, testSet := range testSets {
+		lift, err := Lift(testSet.float64)
+		if err == nil != testSet.error == false {
+			t.Errorf("Unexpected error.\nExpect error: %t\nActual: %s", testSet.error, err)
+		}
+		if !lift.equals(testSet.lift) {
+			t.Errorf("Unexpected value.\nExpected: %s\nActual  : %s", testSet.lift, lift)
 		}
 	}
 }
